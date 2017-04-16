2d93c3945b61ce8d5c2d27445648b0ca3e38dc65
Sarik Kumpan <a5810164@gmail.com>
2017-04-16 22:49:34 +0700

    functional_test test have a time for a day in table this pass

--- bs2560/functional_tests/tests.py
+++ bs2560/functional_tests/tests.py
@@ -87,13 +87,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Hi fox', [row.text for row in head_text])
 
         # he saw the empty table in buttom of this page that show
-        # each row is each day " monday tueday wed .  . .  ."
-        # and each colum was time start in 0.00 - 23.00 each colum has 1 hr
         # in buttom of each row has a button for select 
         table = self.browser.find_element_by_id('table_time')
         rows_day = table.find_elements_by_tag_name('tr')
         first_row = table.find_element_by_id('time')
         colum_time = first_row.find_elements_by_tag_name('th')
+
+        # each row is each day " monday tueday wed .  . .  ."
+ 
+
+        # and each colum was time start in 0.00 - 23.00 each colum has 1 hr
+        for count in range(0, 24):
+            self.assertIn(str(count)+".00", [colum.text for colum in colum_time])
 
         # he saw a inputbox for fill a detail of time
         # he select time 1.00 and 3.00 and fill detail is "Coding",and row of monday
