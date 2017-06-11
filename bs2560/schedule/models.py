9cdeb2ed2563da1152f3489a1fa87d2e35891047
Sarik Kumpan <a5810164@gmail.com>
2017-06-11 16:50:48 +0700

    add email field to models user and change input type for email

--- bs2560/schedule/models.py
+++ bs2560/schedule/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=60)
+    mail = models.EmailField(default="")
     
     def __str__(self):
         return self.name
